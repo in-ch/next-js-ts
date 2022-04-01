afc4a0d413b1b5b83f6abf5b0492547452cd0184
in-ch <sxin2949@naver.com>
2022-04-01 22:04:54 +0900

    Routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import '../styles/globals-style';
+import { useState } from 'react';
 
 const Home: NextPage = () => {
+  const [counter, setCounter] = useState<number>(0);
   return (
-      <p>sdfasdfasdf</p>
+    <>
+      <p>Counter {counter}</p>
+      <button
+        onClick={()=>setCounter((prev)=>prev+1)}
+      >+</button>
+    </>
+      
   )
 }
 
