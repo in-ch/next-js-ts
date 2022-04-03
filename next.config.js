111bcafebe2c2c2183a2f11a04c59bd007995014
in-ch <sxin2949@naver.com>
2022-04-03 18:39:33 +0900

    Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode:true,
+  async redirects(){
+    return [
+      {
+        source:"/contact",  // parameter도 제공 가능.   
+        destination:"/form",  // 외부 사이트도 상관없다. 
+        permanent:false,  // 브라우저나 검색엔진이 기억하는 지 여부 
+      }
+    ]
+  },
+  async rewrites(){  // 주소창이 바뀌지는 않음. (따라서 get으로 요청하는 거 가려줄 수 있음.) -> request를 marking 할 수 있음. 
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      }
+    ]
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import { useEffect, useState } from 'react';
 import Seo from './Seo'
+import styles from "./index.module.css";
 
 interface MovieProps {
   id:number,
   original_title:string,
+  poster_path:string,
 }
 
 const API_KEY = "10923b261ba94d897ac6b81148314a3f";
@@ -22,18 +24,19 @@ const Home: NextPage = () => {
     })();
   }, []);
   return (
-    <>
+    <div className={styles.container}>
       <Seo
         title="Home"
       />
       <br/>
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie:MovieProps) => (
-        <div key={movie.id}>
+        <div className={styles.movie} key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
-    </>
+    </div>
       
   )
 }
