c9ff502f2d85ea4453aebd747f8f1f60308e62f9
in-ch <sxin2949@naver.com>
2022-04-03 18:00:02 +0900

    Fetching Data

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,18 +1,21 @@
-import type { NextPage } from 'next'
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
     const router = useRouter();  // location에 대한 정보가 나옴. 
 
     return (
-        <nav>
-            <Link href="/">
-                <a>Home</a>
-            </Link>
-            <Link href="/about">
-                <a>about</a>
-            </Link>
+        <nav className={styles.nav}>
+            <img className={styles.img} src="/vercel.svg" />
+            <div>
+                <Link href="/">
+                <a className={router.pathname === "/" ? styles.active : ""}>Home</a>
+                </Link>
+                <Link href="/about">
+                <a className={router.pathname === "/about" ? styles.active : ""}>About</a>
+                </Link>
+            </div>
         </nav>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,38 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react';
 import Seo from './Seo'
 
+interface MovieProps {
+  id:number,
+  original_title:string,
+}
 
+const API_KEY = "10923b261ba94d897ac6b81148314a3f";
 
 const Home: NextPage = () => {
+  const [movies, setMovies] = useState<[]>([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <>
       <Seo
         title="Home"
       />
-      <h1>Home</h1>
+      <br/>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie:MovieProps) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </>
       
   )
